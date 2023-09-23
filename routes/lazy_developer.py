1f61a7871bce2995f9db7b9d97264d655afb3fd1
TeoSean00 <teosean@outlook.com>
2023-09-23 14:37:12 +0800

    lazy dev

--- routes/lazy_developer.py
+++ routes/lazy_developer.py
@@ -24,172 +24,77 @@ def evaluate_lazy_developer():
     return json.dumps(res)
 
 
-def getNextProbableWords(classes: List[Dict],
-                         statements: List[str]) -> Dict[str, List[str]]:
+def getNextProbableWords(classes: List[Dict], statements: List[str]) -> Dict[str, List[str]]:
   # Fill in your solution here and return the correct output based on the given input
-  emptyString = set()
-  normalClass = {}
-  polymorphicType = set()
-  enumType = {}
-  keys = []
-  result = {}
-
-  # categorizing all keys in classes to their specific category
-  for json in classes:
-    key = list(json.keys())[0]
-    value = json[key]
-    # populating list of keys in classes
-    keys.append(key)
-    # categorize by type
-    # emptyString
-    if value == "":
-      emptyString.add(key)
-    # class
-    elif isinstance(value, dict):
-      normalClass[key] = value
-
-  for json in classes:
-    key = list(json.keys())[0]
-    value = json[key]
-    # polymorphic or enum type
-    if isinstance(value, list):
-      polymorphicCheck = True
-      for substring in value:
-        if substring not in keys:
-          polymorphicCheck = False
-          break
-      # polymorphic
-      if polymorphicCheck:
-        polymorphicType.add(key)
-      # enum
-      else:
-        enumType[key] = value
+  MAX_NUM_RETURNS: int = 5
+    
+  class_dict = {}  # Dictionary to store class definitions
+  enum_dict = {}   # Dictionary to store enum definitions
+  result = {}      # Dictionary to store the final output
+  polymorphic_classes = set() # List to store classes with polymorphic types
+  none_classes = set() # List to store classes with none types
   
-  # populating result dictionary
-  for statement in statements:
-    # 2 main cases, ends with '.' or does not end with '.'
-    if statement.endswith("."):
-      word = statement[:-1]
-      # single word case
-      if "." not in word:
-        if word in emptyString or word in polymorphicType:
-          result[statement] = [""]
-        elif word in normalClass:
-          value = list(normalClass[word].keys())
-          sortedValue = sorted(value, key=lambda x: x)[:5]
-          result[statement] = sortedValue
-        elif word in enumType:
-          value = enumType[word]
-          sortedValue = sorted(value, key=lambda x: x)[:5]
-          result[statement] = sortedValue
-      # nested words case
-      else:
-        words = word.split(".")
-        # emptystring or polymorphic type
-        if words[0] in emptyString or words[0] in polymorphicType:
-          result[statement] = [""]
-        # normalclass type
-        elif words[0] in normalClass:
-          values = list(normalClass[words[0]].keys())
-          exactMatch = False
-          for value in values:
-            if value == words[1]:
-              exactMatch = True
-              break
-          # 2 cases here, if there's an exactMatch else return the most probable words instead
-          if exactMatch:
-            type = normalClass[words[0]][words[1]]
-            if "<" in type:
-              startIndex = type.index("<")
-              endIndex = type.index(">")
-              type = type[startIndex+1:endIndex]
-            # After applying the necessary conversion to type, we look for type, type cant be found in normalClass too
-            if type in emptyString or type in polymorphicType:
-              result[statement] = [""]
-            elif type in enumType:
-              value = enumType[type]
-              sortedValue = sorted(value, key=lambda x: x)[:5]
-              result[statement] = sortedValue
-          # if no exactMatch, return most probable words instead
-          else:
-            temp = []
-            for value in values:
-              if value.startswith(words[1]):
-                temp.append(value)
-            sortedValue = sorted(temp, key=lambda x: x)[:5]
-            result[statement] = sortedValue
-        # enum type
-        elif words[0] in enumType:
-          values = enumType[words[0]]
-          temp = []
-          for value in values:
-            if value.startswith(words[1]):
-              temp.append(value)
-          sortedTemp = sorted(temp, key=lambda x: x)[:5]
-          result[statement] = sortedTemp
-          
-    # other main case, where statement does not end with "."
-    else:
-      words = statement.split(".")
-      # case where only 1 word
-      if len(words) == 1:
-        word = words[0]
-        # emptystring or polymorphic type
-        if word in emptyString or word in polymorphicType:
-          result[statement] = [""]
-        # normalclass type
-        elif word in normalClass:
-          value = list(normalClass[word].keys())
-          sortedValue = sorted(value, key=lambda x: x)[:5]
-          result[statement] = sortedValue
-        # enumtype
-        elif word in enumType:
-          value = enumType[word]
-          sortedValue = sorted(value, key=lambda x: x)[:5]
-          result[statement] = sortedValue
-      # case with multiple words
-      else:
-        # emptystring or polymorphic type
-        if words[0] in emptyString or words[0] in polymorphicType:
-          result[statement] = [""]
-        # normalclass type
-        elif words[0] in normalClass:
-          values = list(normalClass[words[0]].keys())
-          exactMatch = False
-          for value in values:
-            if value == words[1]:
-              exactMatch = True
-              break
-          # 2 cases here, if there's an exactMatch else return the most probable words instead
-          if exactMatch:
-            type = normalClass[words[0]][words[1]]
-            if "<" in type:
-              startIndex = type.index("<")
-              endIndex = type.index(">")
-              type = type[startIndex+1:endIndex]
-            # After applying the necessary slicing to type if needed
-            if type in emptyString or type in polymorphicType:
-              result[statement] = [""]
-            elif type in enumType:
-              value = enumType[type]
-              sortedValue = sorted(value, key=lambda x: x)[:5]
-              result[statement] = sortedValue
-          # if no exactMatch, return most probable words instead
-          else:
-            temp = []
-            for value in values:
-              if value.startswith(words[1]):
-                temp.append(value)
-            sortedValue = sorted(temp, key=lambda x: x)[:5]
-            result[statement] = sortedValue
-        # enum type
-        elif words[0] in enumType:
-          values = enumType[words[0]]
-          temp = []
-          for value in values:
-            if value.startswith(words[1]):
-              temp.append(value)
-          sortedTemp = sorted(temp, key=lambda x: x)[:5]
-          result[statement] = sortedTemp
+  class_names = [] # List to store class names
+  for subclass in classes:
+      class_names.append(list(subclass.keys())[0])
+  
+  # Process class and enum definitions
+  for class_info in classes:
+      key = next(iter(class_info))
+      value = class_info[key]
 
+      if value == '':
+          none_classes.add(key)
+      elif isinstance(value, list):  # Enum
+          if any(class_name in value for class_name in class_names):
+              polymorphic_classes.add(key)
+          else:
+              enum_dict[key] = value
+      elif isinstance(value, dict):  # Class
+          class_dict[key] = list(value.keys())
+  
+  # Process interior types
+  for class_info in classes:
+      key = next(iter(class_info))
+      value = class_info[key]
+
+      if isinstance(value, dict):  # Class
+          for v_key, v_value in value.items():
+              if any(polymorphic_class_string in v_value for polymorphic_class_string in polymorphic_classes):
+                  polymorphic_classes.add(f"{key}.{v_key}")
+  
+  # Process statements
+  for statement in statements:
+      if statement.endswith('.'):
+          prefix = statement[:-1]
+
+          if prefix in polymorphic_classes or prefix in none_classes:
+              result[statement] = [""]
+              continue
+          elif prefix in class_dict:
+              # matches = [key for key in class_dict if key.startswith(prefix)]
+              matches = class_dict[prefix]
+          elif prefix in enum_dict:
+              # matches = [key for key in enum_dict if key.startswith(prefix)]
+              matches = enum_dict[prefix]
+
+          probable_words = [word for word in matches]
+          probable_words.sort()
+          result[statement] = probable_words[:MAX_NUM_RETURNS]
+      else:
+          if statement in polymorphic_classes or statement in none_classes:
+              result[statement] = [""]
+              continue
+
+          class_name, attribute = statement.split('.')
+
+          if class_name in class_dict:
+              probable_words = [word for word in class_dict[class_name] if word.startswith(attribute)]
+              probable_words.sort()
+              result[statement] = probable_words[:MAX_NUM_RETURNS]
+          elif class_name in enum_dict:
+              probable_words = [word for word in enum_dict[class_name] if word.startswith(attribute)]
+              probable_words.sort()
+              result[statement] = probable_words[:MAX_NUM_RETURNS]
+  
   return result
